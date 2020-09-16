f0fede3c0645b6733a2aee4af190d9a9f7450dd3
joohyung <qweasd_147@naver.com>
2020-09-16 12:25:58 +0900

    zuul에서 토큰을 2번 파싱하게 되던 버그 수정

--- api-gateway/src/main/java/com/zuul/config/ZuulContextConfig.java
+++ api-gateway/src/main/java/com/zuul/config/ZuulContextConfig.java
@@ -6,19 +6,19 @@ import com.netflix.zuul.exception.ZuulException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class ZuulContextConfig extends ZuulFilter {
 
-    private final ResourceServerTokenServices tokenServices;
     private static final String TOKEN_HEADER = "Bearer ";
 
 
@@ -40,32 +40,23 @@ public class ZuulContextConfig extends ZuulFilter {
 
         if(StringUtils.isEmpty(token))  return false;
 
-        return token.startsWith(TOKEN_HEADER);
+        return token.startsWith(TOKEN_HEADER) && !Objects.isNull(getClaims());
     }
 
     @Override
     public Object run() throws ZuulException {
 
-        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-        String tokenHeaderVal = request.getHeader("Authorization");
-
-        if(StringUtils.isEmpty(tokenHeaderVal))  return null;
-
-        final String accessToken = tokenHeaderVal.substring(TOKEN_HEADER.length());
-        final String userName;
-
-        try{
-            OAuth2Authentication claims = tokenServices.loadAuthentication(accessToken);
-            userName = claims.getName();
-        }catch (RuntimeException ex){
-            log.info("토큰 파싱 에러 토큰 만료됬을 가능성이 제일 높음. {}", accessToken);
-            return null;
-        }
-
         RequestContext.getCurrentContext()
-                .addZuulRequestHeader("username", userName);
+                .addZuulRequestHeader("username", getClaims().getName());
 
         return null;
+    }
+
+    private OAuth2Authentication getClaims(){
+
+        return (OAuth2Authentication) SecurityContextHolder
+                .getContext()
+                .getAuthentication();
     }
 
 }
